ae09fec7bc0e4128c0925058db4e26d7a4180616
Диана <dianamingazova162@gmail.com>
2022-12-21 19:13:03 +0300

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println(" ");
+        System.out.println("Задача 1");
+        String firsttName = "Иван ";
+        String middleName = "Иванович ";
+        String lastName = "Иванов ";
+        String fullName = lastName + firsttName + middleName;
+        System.out.println( "ФИО сотрудника — " + fullName);
 
+        System.out.println(" ");
+        System.out.println("Задача 2");
+        fullName = fullName.replace("Иванов Иван Иванович ", "ИВАНОВ ИВАН ИВАНОВИЧ" );
+        System.out.println( "Данные ФИО сотрудника для заполнения отчета — " + fullName);
+
+        System.out.println(" ");
+        System.out.println("Задача 3");
+        String fulllName = "Иванов Семён Семёнович";
+        fulllName = fulllName.replace("ё", "е");
+        System.out.println( "Данные ФИО сотрудника — "+fulllName);
     }
 }
